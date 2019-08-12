a2b2be8115e96e82686a18b80f461da7fd6b7acb
YeQingA <rockingstone666@163.com>
2019-08-12 14:12:34 +0800

    Initial commit

---
+++ src/main/java/com/rock/mycards/MycardsApplication.java
@@ -0,0 +1,13 @@
+package com.rock.mycards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycardsApplication.class, args);
+    }
+
+}
